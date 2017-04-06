84a61fe852e633f3e02da452bf77ee514f163885
ashraf <ashraf1abdelrasool@gmail.com>
2017-04-06 18:20:12 +0200

    Initial commit

---
+++ src/main/java/org/revo/WeboxjApplication.java
@@ -0,0 +1,11 @@
+package org.revo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeboxjApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WeboxjApplication.class, args);
+    }
+}
